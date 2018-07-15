3791628e97fb0ebd1fc84cc978b6ef9d7425f416
NYKIM <nykim@nykim.net>
2018-07-15 19:56:22 +0900

    add service slide pagination effect

--- app/src/js/script/slideJS.js
+++ app/src/js/script/slideJS.js
@@ -18,7 +18,7 @@ var visualSwiper = new Swiper(".visual-slide", {
 });
 
 // *** 비주얼 슬라이드
-var serviceSwiperDuration = 5000;
+var serviceSwiperDuration = 8000;
 var serviceSwiper = new Swiper(".service-slide", {
   // Optional parameters
   loop: true,
@@ -44,62 +44,80 @@ var serviceSwiper = new Swiper(".service-slide", {
   watchSlidesProgress: true
 });
 
-var progressBar = new TimelineMax({
-  // onComplete: function() {
-  //   this.restart();
-  // }
-});
-
-progressBar
-  .from(".line", 5, { width: 0, ease: Linear.easeNone }, 0)
-  .timeScale(1);
-
+//서비스 영역 슬라이드 이벤트는 모두 여기에 정의
 var serviceSlideEvent = function() {
+  //변수 선언
   var $pagination_current = $(".pagination-text.current");
   var $pagination_entire = $(".pagination-text.entire");
-  var $line = $(".service-slide-progress .line");
+  var progressBar = new TimelineMax();
+  var _tl_pagination = new TimelineMax();
 
+  //프로그레스바 타임라인 정의
+  progressBar
+    .from(
+      ".service-slide-progress .line", //대상
+      8, //시간
+      { width: 0, ease: Linear.easeNone }, //0프레임일 때 사항
+      0
+    )
+    .timeScale(1);
+
+  //페이지네이션 타임라인 정의
+
+  var counter = $(".service-slide-counter");
+  var currentCount = $('<span class="count">1<span/>');
+  counter.append(currentCount);
+
+  function paginationUpdate() {
+    var index = serviceSwiper.realIndex + 1,
+      $current = $(".service-slide").eq(index),
+      dur = 0.8;
+
+    var prevCount = $(".count");
+    currentCount = $('<span class="count next">' + index + "<span/>");
+    currentCount.appendTo(counter);
+    TweenLite.to(prevCount, dur, {
+      y: -12,
+      opacity: 0,
+      onCompleteParams: [prevCount],
+      onComplete: function(prevCount) {
+        prevCount.remove();
+      },
+      ease: Power2.easeOut
+    });
+    TweenLite.fromTo(
+      currentCount,
+      dur,
+      {
+        y: 12,
+        opacity: 0
+      },
+      {
+        y: 0,
+        opacity: 1,
+        ease: Power2.easeOut
+      }
+    );
+  }
+
+  //전체 슬라이드 개수 표시
   $pagination_entire.text(serviceSwiper.slides.length - 2);
 
-  //페이지네이션 숫자 변경
+  //슬라이드 바뀔 때마다 페이지네이션 숫자 변경 및 프로그레스바 리셋
   serviceSwiper.on("slideChange", function() {
     var slideIndex = serviceSwiper.realIndex;
-    $pagination_current.text(slideIndex + 1);
+    // $pagination_current.text(slideIndex + 1);
+    paginationUpdate();
     progressBar.restart();
   });
-  serviceSwiper.on("progress", function() {
-    // setProgressBar($line, serviceSwiperDuration);
-  });
-
-  //프로그레스바 반영
-  function setProgressBar(target, duration) {
-    var $elem = target;
-    var width = 1;
-    var playTime = duration / 100;
-    var intervalID = setInterval(frame, playTime);
-    function frame() {
-      if (width >= 100) {
-        width = 0;
-        clearInterval(intervalID);
-      } else {
-        width++;
-        console.log("~~~width,", width);
-        var scaleX = width * 0.01;
-        // $elem.css({ width: width + "%" });
-        $elem.css({ transform: "scaleX(" + width * 0.01 + ")" });
-      }
-    }
-  }
 };
 
-setInterval(function() {
-  console.log(serviceSwiper.autoplay.running);
-}, 1000);
-
+// 윈도 로드 시 이벤트 정의
 $(window).on("load", function() {
   serviceSlideEvent();
 });
 
+/////삭제예정////
 var tl = new TimelineMax({
   paused: true
 });
@@ -119,14 +137,7 @@ $(".card").on("mouseleave", function() {
   console.log("!");
   tl.reverse();
 });
-
-var parent = new TimelineMax({
-  onComplete: function() {
-    this.restart();
-  }
-});
-
-parent.from(".red", 3, { width: 0, ease: Linear.easeNone }, 0).timeScale(2);
+/////삭제예정////
 
 // // ** 어바웃 슬라이드
 // var aboutSlide = function() {
